c788809f43c2707db4a8d751d9638d30f30cca65
nathanjh-28 <nathan@nathanjharris.com>
2022-06-21 09:44:58 -0500

    updated code to reflect course and prep for exercise

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,26 @@ import trollFace from '../Troll Face.png'
 export default function Header() {
     return (
         <header className="header">
-            <div className="nav-left">
-                <img src={trollFace} alt="" />
-                <h2>MemeGenerator</h2>
-            </div>
-            <div className="nav-right">
-                <p>React Course - Project 3</p>
-            </div>
+            <img
+                src={trollFace}
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
         </header>
     )
 }
+
+// export default function Header() {
+//     return (
+//         <header className="header">
+//             <div className="nav-left">
+//                 <img src={trollFace} alt="" />
+//                 <h2>MemeGenerator</h2>
+//             </div>
+//             <div className="nav-right">
+//                 <p>React Course - Project 3</p>
+//             </div>
+//         </header>
+//     )
+// }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,45 @@
 import React from "react";
 
+
 export default function Meme() {
+    /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
     return (
-        <div className="meme-form">
-            <div className="meme-inputs">
-                <input type="text" placeholder="top text" />
-                <input type="text" placeholder="bottom text" />
+        <main>
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
             </div>
-            <button className="submit" type="submit">Get New Image</button>
-        </div>
+        </main>
     )
 }
+
+// export default function Meme() {
+//     return (
+//         <div className="meme-form">
+//             <div className="meme-inputs">
+//                 <input type="text" placeholder="top text" />
+//                 <input type="text" placeholder="bottom text" />
+//             </div>
+//             <button className="submit" type="submit">Get New Image</button>
+//         </div>
+//     )
+// }
