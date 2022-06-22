429781818910ec37f512bc74b053b6e69822ab72
nathanjh-28 <nathan@nathanjharris.com>
2022-06-22 12:46:20 -0500

    polished code and added comment headers

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,15 +4,18 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
-    // -- State
-    //const [imgUrl, setImgUrl] = React.useState('http://i.imgflip.com/1bij.jpg')
+    // ______________________Meme State
     const [meme, setMeme] = React.useState({
         topText: '',
         bottomText: '',
         randomImage: 'http://i.imgflip.com/1bij.jpg'
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
+    // ______________________All Memes State
+    const [allMemeImages] = React.useState(memesData)
+
+
+    // ______________________Event Handler
     function handleClickEvent() {
         const memesArray = allMemeImages.data.memes
         const randomIdx = Math.floor((Math.random() * memesArray.length));
@@ -23,9 +26,10 @@ export default function Meme() {
                 randomImage: url
             }
         })
-        console.log(meme)
     }
 
+
+    // ______________________JSX
     return (
         <main>
             <div className="form">
@@ -52,15 +56,3 @@ export default function Meme() {
         </main>
     )
 }
-
-// export default function Meme() {
-//     return (
-//         <div className="meme-form">
-//             <div className="meme-inputs">
-//                 <input type="text" placeholder="top text" />
-//                 <input type="text" placeholder="bottom text" />
-//             </div>
-//             <button className="submit" type="submit">Get New Image</button>
-//         </div>
-//     )
-// }
